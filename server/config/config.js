2f1cb9e6098d0d963067a207cdd39dd052625658
jeisson <carrillojeisson2@gmail.com>
2020-02-17 17:43:21 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,17 @@
 
 // puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDb
+
+if( process.env.NODE_ENV == 'dev') {
+    urlDb = 'mongodb://localhost:27017/cafe2';
+} else {
+    urlDb = 'mongodb+srv://cafe-user:wpWSEI8PTUY0FZ95@cluster0-wuemn.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDb;
