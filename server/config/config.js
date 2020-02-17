a25dd439a5556895fc5d170cb28d75d6986d846b
jeisson <carrillojeisson2@gmail.com>
2020-02-17 18:26:35 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,8 @@ let urlDb
 if( process.env.NODE_ENV == 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe2';
 } else {
-    urlDb = 'mongodb+srv://cafe-user:wpWSEI8PTUY0FZ95@cluster0-wuemn.mongodb.net/test?retryWrites=true&w=majority';
+    urlDb = process.env.MONGO_URI;
+    // 'mongodb+srv://cafe-user:wpWSEI8PTUY0FZ95@cluster0-wuemn.mongodb.net/test?retryWrites=true&w=majority'
 }
 
 process.env.URLDB = urlDb;
